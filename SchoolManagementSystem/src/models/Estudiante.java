d0bdf7bb03d862d9c2020e976bc1d65e0ff70fdc
omonterroso-2022141 <omonterroso-2022141@kinal.edu.gt>
2024-09-09 03:01:11 -0600

    feat: The models were created - Added the models (Student, Course, Academic Manager) - An ArrayList was used to store students, another ArrayList for courses, and a HashMap was used to maintain the list of students enrolled in each course

---
+++ SchoolManagementSystem/src/models/Estudiante.java
@@ -0,0 +1,69 @@
+package models;
+
+public class Estudiante {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String fechaDeNacimiento;
+    private String estado; // matriculado, inactivo, graduado
+
+    public Estudiante(int id, String nombre, String apellido, String fechaDeNacimiento, String estado) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaDeNacimiento = fechaDeNacimiento;
+        this.estado = estado;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getFechaDeNacimiento() {
+        return fechaDeNacimiento;
+    }
+
+    public void setFechaDeNacimiento(String fechaDeNacimiento) {
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", fechaDeNacimiento='" + fechaDeNacimiento + '\'' +
+                ", estado='" + estado + '\'' +
+                '}';
+    }
+}
